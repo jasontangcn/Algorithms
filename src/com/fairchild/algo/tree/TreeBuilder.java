3d035e65165c323a382ddd159be2133eb02bd9de
Jason Tang <TomHornson@hotmail.com>
2014-09-18 08:28:19 +0800

    + TreeBuilder

---
+++ src/com/fairchild/algo/tree/TreeBuilder.java
@@ -0,0 +1,34 @@
+package com.fairchild.algo.tree;
+
+public class TreeBuilder {
+	public static TreeNode build(char[] preOrder, int i, char[] midOrder, int j, int n) {
+		if (n <= 0)
+			return null;
+
+		TreeNode node = new TreeNode();
+		node.setData(preOrder[i]);
+
+		int k;
+		for (k = j; k < n; k++) {
+			if (midOrder[k] == preOrder[i])
+				break;
+		}
+
+		node.setLeftChild(build(preOrder, i + 1, midOrder, j, k - j));
+		node.setRightChild(build(preOrder, i + k - j + 1, midOrder, k + 1, n - (k - j) - 1));
+
+		return node;
+	}
+
+	public static void printTree(TreeNode root) {
+		System.out.println(root.getData());
+		if (null != root.getLeftChild())
+			printTree(root.getLeftChild());
+		if (null != root.getRightChild())
+			printTree(root.getRightChild());
+	}
+
+	public static void main(String[] args) {
+		printTree(build(new char[]{'A','B','D','E','C','F','G'},0, new char[]{'D','B','E','A','C','G','F'},0,7));
+	}
+}
---
+++ src/com/fairchild/algo/tree/TreeNode.java
@@ -0,0 +1,26 @@
+package com.fairchild.algo.tree;
+
+public class TreeNode {
+	private Object data;
+	private TreeNode leftChild;
+	private TreeNode rightChild;
+	
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+	public TreeNode getLeftChild() {
+		return leftChild;
+	}
+	public void setLeftChild(TreeNode leftChild) {
+		this.leftChild = leftChild;
+	}
+	public TreeNode getRightChild() {
+		return rightChild;
+	}
+	public void setRightChild(TreeNode rightChild) {
+		this.rightChild = rightChild;
+	}
+}
